e28d3a2c9b651d35bba8581c6a43b1a356a4d71c
vmwavie <vmwavie@protonmail.com>
2024-02-13 13:19:21 -0300

    [test] ci/cd testing

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { env } from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(env.PORT);
 }
 bootstrap();
